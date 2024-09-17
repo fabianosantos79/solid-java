2c22a2d796ff464f60226f571e12b26a5e1e4aeb
Fabiano <fabiano.ssantos79@gmail.com>
2024-09-17 14:34:44 -0300

    feat: init project

---
+++ 3149-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -0,0 +1,65 @@
+package br.com.alura;
+
+import br.com.alura.services.AbrigoService;
+import br.com.alura.services.PetsService;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class AdopetConsoleApplication {
+
+
+
+    public static void main(String[] args) {
+        AbrigoService abrigoService = new AbrigoService();
+        PetsService petsService = new PetsService();
+
+        System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
+        try {
+            int opcaoEscolhida = 0;
+            while (opcaoEscolhida != 5) {
+                System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+                System.out.println("1 -> Listar abrigos cadastrados");
+                System.out.println("2 -> Cadastrar novo abrigo");
+                System.out.println("3 -> Listar pets do abrigo");
+                System.out.println("4 -> Importar pets do abrigo");
+                System.out.println("5 -> Sair");
+
+                String textoDigitado = new Scanner(System.in).nextLine();
+                opcaoEscolhida = Integer.parseInt(textoDigitado);
+
+                if (opcaoEscolhida == 1) {
+                    abrigoService.listarAbrigos();
+                } else if (opcaoEscolhida == 2) {
+                    abrigoService.cadastrarAbrigo();
+                } else if (opcaoEscolhida == 3) {
+                    petsService.listarPets();
+                } else if (opcaoEscolhida == 4) {
+                    petsService.importarPets();
+                } else if (opcaoEscolhida == 5) {
+                    break;
+                } else {
+                    System.out.println("NÚMERO INVÁLIDO!");
+                    opcaoEscolhida = 0;
+                }
+            }
+            System.out.println("Finalizando o programa...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
